436771ef41046c97c7538cc9a8c9e3efdf4267f1
pb6969 <165070191+pb6969@users.noreply.github.com>
2024-06-17 17:31:02 +0530

    project added

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workouts');
+const app = express();
+const uri='mongodb+srv://admin:admin123@mernapp.jgcdgyl.mongodb.net/?retryWrites=true&w=majority&appName=MERNapp';
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+});
+
+
+app.use('/api/workouts', workoutRoutes);
+
+
+mongoose.connect(uri)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(4000, () => {
+      console.log('listening for requests on port', 4000)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  });
